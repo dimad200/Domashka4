1262681d994e0554ae79325c0c14212f53b4d431
dimad200 <dimad@udm.ru>
2024-11-24 20:35:52 +0400

    Домашка для 5-го модуля

---
+++ module_5_3.py
@@ -0,0 +1,91 @@
+# Необходимо дополнить класс House следующими специальными методами:
+#
+# __eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.
+# Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=) должны присутствовать в классе и возвращать
+# результаты сравнения по соответствующим операторам. Как и в методе __eq__ в сравнении участвует кол-во этажей.
+# __add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
+# __radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
+# Остальные методы арифметических операторов, где self - x, other - y:
+#
+
+class House:
+    def __init__(self,name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+
+
+    def __str__(self):
+        return str(f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+
+    def go_to(self,new_floor):
+        # проверяю что значение new_floor не выходит за условия задачи
+        if  new_floor < 0 or new_floor > self.number_of_floors :
+            print("Такого этажа не существует")
+        else:
+            for i in (range(1,new_floor+1)):
+                print(i)
+
+# Добавление новых методов
+    def __eq__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+        else:
+            return False
+        # тут False так как проверка на равенство
+        # если тип данных другой значит не ровны вернуть False
+
+    def __lt__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+        else:
+            return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+        else:
+            return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+        else:
+            return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+        else:
+            return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+        else:
+            return True
+            # тут True так как проверка на неравенство
+            # если тип другой значит не ровны вернуть True
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+print(h1 ==h2) # __eq__
