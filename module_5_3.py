8d77b425cedc8ed98d9d2bca9a928f249a0fe488
dimad200 <dimad@udm.ru>
2024-11-24 22:06:37 +0400

    Домашка для 5-го модуля

--- module_5_3.py
+++ module_5_3.py
@@ -81,11 +81,61 @@ class House:
         else:
             return True
             # тут True так как проверка на неравенство
-            # если тип другой значит не ровны вернуть True
+            # если тип данных другой значит не ровны вернуть True
+
+    def __add__(self, other):
+        if isinstance(other,House):
+            self.number_of_floors =self.number_of_floors + other.number_of_floors
+
+            return self
+        elif isinstance(other, int):
+            self.number_of_floors = self.number_of_floors + other
+
+            return self
+        else:
+            return NotImplemented
+
+    def __iadd__(self, other):
+        if isinstance(other,House):
+            self.number_of_floors += other.number_of_floors
+            return self
+        elif isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        else:
+            return NotImplemented
+
+    def __radd__(self, other):
+        self.number_of_floors = self.number_of_floors + other
+        return self
+
+
+
+# class MyClass:
+#     def __init__(self, value):
+#         self.value = value
+#     def __add__(self, other):
+#         return MyClass(self.value + other.value)
+
 
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
-print(h1 ==h2) # __eq__
+print(h1 == h2) # __eq__
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+h1 += 10 # __iadd__
+print("11111")
+print(h1)
+h2 = 10 + h2 # __radd__
+print(h2)
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+
+
