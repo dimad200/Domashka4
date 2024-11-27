cd10a9d20eb48b6c7a2db7a4e614efbea120b5ef
dimad200 <dimad@udm.ru>
2024-11-27 16:42:52 +0400

    Домашка для 5-го модуля

--- module_5_4.py
+++ module_5_4.py
@@ -1,17 +1,3 @@
-class Example:
-    def __new__(cls, *args, **kwargs):
-        print(args)
-        print(kwargs)
-        return object.__new__(cls)
-
-    def __init__(self, first, second, third):
-        print(first)
-        print(second)
-        print(third)
-
-ex = Example('data', second=25, third=3.14)
-
-
 class House:
     houses_history = []
 
