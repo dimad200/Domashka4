377ed8cf71b5e3d79f4f08c350e7411b3eb7178c
dimad200 <dimad@udm.ru>
2024-12-03 17:53:35 +0400

    Добавил модуль 6_1

--- module_6_3.py
+++ module_6_3.py
@@ -7,11 +7,15 @@ class Animal:
         self._cords = [0, 0, 0]
         self.speed = speed
 
-    def move(self, dx, dy, dz)
+    def move(self, dx, dy, dz):
         self._cords[0] = self._cords[0] + dx * self.speed
         self._cords[1] = self._cords[1] + dy * self.speed
-        self._cords[2] = self._cords[1] + dz * self.speed
-
+        if self._cords[2] + dz * self.speed < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+            self._cords[2] = self._cords[2] + dz * self.speed
+    def get_cords(self):
+        print(f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}")
 
 
 a1 = Animal(10)
