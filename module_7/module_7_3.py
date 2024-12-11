656fb2d01e967e6718bfeba37a4e6d8277d447c2
dimad200 <dimad@udm.ru>
2024-12-11 21:51:57 +0400

    добавлен модуль 7_3

--- module_7/module_7_3.py
+++ module_7/module_7_3.py
@@ -14,7 +14,7 @@ class WordsFinder():
                 a = file.read()
                 a = a.lower()
                 for k in replace:
-                    a = a.replace(k, "")
+                    a = a.replace(k, '')
                 a = a.split()
             all_words[i] = a
         return all_words
@@ -28,7 +28,15 @@ class WordsFinder():
                 if self.word == v[i]:
                     znachenie = i + 1
                     return {k: znachenie}
-
+    def count(self, word):
+        self.word = word.lower()
+        znachenie = 0
+        slowar = self.get_all_words()
+        for k, v in slowar.items():
+            for i in range(len(v)):
+                if self.word == v[i]:
+                    znachenie+=1
+        return {k: znachenie}
 
 
 
@@ -39,10 +47,8 @@ class WordsFinder():
 # В методах find и count пользуйтесь ранее написанным методом get_all_words для получения названия файла и списка его слов.
 # Для удобного перебора одновременно ключа(названия) и значения(списка слов) можно воспользоваться методом словаря - item(). a
 
-
-a= WordsFinder('file1.txt, file2.txt', 'file3.txt')
-print(a.file_names)
-finder2 = WordsFinder('test_file.txt','test_file2.txt')
+finder2 = WordsFinder('test_file.txt')
 
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
