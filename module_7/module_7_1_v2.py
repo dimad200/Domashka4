773bac2af27faf17770ab5ba33bbf537bd721fc2
dimad200 <dimad@udm.ru>
2024-12-11 20:22:47 +0400

    Исправил 5_Hard

--- module_7/module_7_1_v2.py
+++ module_7/module_7_1_v2.py
@@ -1,11 +1,11 @@
-from os.path import split
 from pprint import pprint
 
+
 class Product():
-    def __init__(self, name, weight,category):
+    def __init__(self, name, weight, category):
         self.name = str(name)
         self.weight = float(weight)
-        self.category=str(category)
+        self.category = str(category)
 
     def __str__(self):
         return "{}, {}, {}".format(self.name, self.weight, self.category)
@@ -15,12 +15,6 @@ class Shop():
 
     def __init__(self):
         self.__file_name = 'products.txt'
-
-    def get_products(self):
-        file = open(self.__file_name, 'r')
-        result_str=file.read()
-        file.close()
-        return result_str
 
     def get_products(self):
         file = open(self.__file_name, 'r')
@@ -37,33 +31,18 @@ class Shop():
                 print(f"Продукт {i.name} уже есть в магазине")
             else:
                 file.write(str(i) + "\n")
+
         file.close()
 
 
 
 
-
-
-
-        # for i in str_:
-        #     str_1=i.split(", ")
-        #     str_1=str_1[0]
-        #     print (str_1)
-        #     for k in products:
-        #         if str_1==k.name:
-        #             print("такой есть")
-        #             break
-        #         else:
-
-
-
-
-
-            # Проверка:
+# Проверка:
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
+
 print(p2)  # __str__
 s1.add(p1, p2, p3)
 print(s1.get_products())
