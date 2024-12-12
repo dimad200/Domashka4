8eaa24441481bebb805b89a4ce85df7f221ba29f
dimad200 <dimad@udm.ru>
2024-12-12 17:55:03 +0400

    добавлен модуль 7_4

--- module_7/module_7_4.py
+++ module_7/module_7_4.py
@@ -8,17 +8,12 @@ tasks_total =score_1+score_2
 time_avg = (team1_time+team2_time)/score_2
 challenge_result = 'Победа команды Волшебники данных!'
 
-
-
-
 print("В команде Мастера кода участников: %s !" % (team1_num))
 print("Итого сегодня в командах участников: %s и %s !" %( team1_num, team2_num))
 print("Команда Волшебники данных решила задач: {} !" .format(score_2))
 print("Волшебники данных решили задачи за {} с !".format(team1_time))
 print(f"Команды решили {score_1} и {score_2} задач.")
 
-
-# Переменные: количество задач (tasks_total) и среднее время решения (time_avg).
 print(f"Сегодня было решено {tasks_total} задач, в среднем по {round(time_avg,2)} секунды на задачу!.")
 if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
     result = 'Победа команды Мастера кода!'
@@ -28,5 +23,6 @@ else:
     result ='Ничья!'
 
 challenge_result=result
+
 
 print(f"Результат битвы: {challenge_result}")
