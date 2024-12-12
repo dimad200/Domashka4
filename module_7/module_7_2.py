f9083636ff571a9ca0e6061314207e9480fdc352
dimad200 <dimad@udm.ru>
2024-12-12 16:43:14 +0400

    добавлен модуль 7_3

--- module_7/module_7_2.py
+++ module_7/module_7_2.py
@@ -1,20 +1,16 @@
-from itertools import count
-
-
 def custom_write(file_name, strings):
-    file_name =file_name
-    strings=strings
-    strings_positions= {}
+    strings_positions = {}
 
-    file = open(file_name, 'a',encoding="utf-8")
+    file = open(file_name, 'w', encoding="utf-8")
 
-    count_strings=0
+    count_strings = 0
     for s in strings:
-        count_strings+=1
-        strings_positions[(count_strings,file.tell())]=s
-        file.write(s+"\n")
+        count_strings += 1
+        strings_positions[(count_strings, file.tell())] = s
+        file.write(s + "\n")
     file.close()
     return strings_positions
+
 # Проверка
 info = [
     'Text for tell.',
