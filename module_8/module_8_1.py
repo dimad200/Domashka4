a60b67110a9c87aefaee66d2fa5f2a364e2c37a0
master <dimad@udm.ru>
2024-12-18 10:02:23 +0400

    5_4

---
+++ module_8/module_8_1.py
@@ -0,0 +1,15 @@
+def add_everything_up(a, b):
+    try:
+        result= a+b
+    except TypeError:
+        a=str(a)
+        b=str(b)
+    finally:
+        result = a + b
+        return result
+# Проверка
+print(add_everything_up(123.456, 'строка'))
+
+print(add_everything_up('яблоко', 4215))
+
+print(add_everything_up(123.456, 7))
