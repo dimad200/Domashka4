b9382cdcbb3f7b630a42cb10e3a66bd856908bae
master <dimad@udm.ru>
2024-12-24 14:12:09 +0400

    8_1

---
+++ для мусора/1.py
@@ -0,0 +1,7 @@
+import sys
+sys.stderr.write("Это будет восприниматься как ошибка")
+# sys.stdout.write("Hello, world!")
+#
+# sys.stderr.write("Это будет восприниматься как ошибка")
+a=sys.stdin.read()
+print(a)
