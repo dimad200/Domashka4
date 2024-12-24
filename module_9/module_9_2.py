2e7ed7c5a0f779a8ee49a1249da26f1d7eb43f82
master <dimad@udm.ru>
2024-12-24 14:12:40 +0400

    9_2

--- module_9/module_9_2.py
+++ module_9/module_9_2.py
@@ -6,7 +6,8 @@ first_result = [len(element_iz_first_strings) for element_iz_first_strings in fi
                 len(element_iz_first_strings) > 5]
 second_result = [(pervoe, vtoroe) for pervoe in first_strings for vtoroe in second_strings if
                  len(pervoe) == len(vtoroe)]
-third_result ={stroka:len(stroka)  for stroka in first_strings + second_strings if not len(stroka)%2
+third_result = {
+    stroka: len(stroka) for stroka in first_strings + second_strings if not len(stroka) % 2
 
 }
 print(first_result)
