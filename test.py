f22215441749090adf32355452f4204387f4315f
dimad200 <dimad@udm.ru>
2024-12-24 17:33:20 +0400

    добавлен 9_1

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-class User:
-    def __init__(self,nickname,password,age):
-        self.age=age
-        self.nickname=nickname
-        self.password=hash(password)
-        print(password)
-        print(hash(password))
-        print(hash(password))
-        print(hash(password))
-        print(hash(password))
-
-
-
-user_vasy=User("VASY","сапог",15)
+import tkinter
+print(tkinter.__file__)
