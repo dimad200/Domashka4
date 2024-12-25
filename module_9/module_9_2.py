85e04d34d97218fcb9269d5d4e82564024c941d2
master <dimad@udm.ru>
2024-12-25 16:16:49 +0400

    9_3

--- module_9/module_9_2.py
+++ module_9/module_9_2.py
@@ -3,7 +3,7 @@ first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 # В переменную first_result запишите список созданный при помощи сборки состоящий из длин строк списка first_strings, при условии, что длина строк не менее 5 символов.
 first_result = [len(element_iz_first_strings) for element_iz_first_strings in first_strings if
-                len(element_iz_first_strings) > 5]
+                len(element_iz_first_strings) >= 5]
 second_result = [(pervoe, vtoroe) for pervoe in first_strings for vtoroe in second_strings if
                  len(pervoe) == len(vtoroe)]
 third_result = {
