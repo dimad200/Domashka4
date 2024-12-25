a866e8d8029bae55b33da9c7cd3d727a71a88839
master <dimad@udm.ru>
2024-12-25 16:14:52 +0400

    9_2

--- для мусора/3.py
+++ для мусора/3.py
@@ -1,26 +1,49 @@
 import pygame,sys
-from pygame.locals import *
 
-def sprite_sheet(size,file,pos=(0,0)):
 
-    #Initial Values
-    len_sprt_x,len_sprt_y = size #sprite size
-    sprt_rect_x,sprt_rect_y = pos #where to find first sprite on sheet
-
-    sheet = pygame.image.load(file).convert_alpha() #Load the sheet
-    sheet_rect = sheet.get_rect()
+def sprite_map(file,size_x,size_y):
     sprites = []
-    print sheet_rect.height, sheet_rect.width
-    for i in range(0,sheet_rect.height-len_sprt_y,size[1]):#rows
-        print "row"
-        for i in range(0,sheet_rect.width-len_sprt_x,size[0]):#columns
-            print "column"
-            sheet.set_clip(pygame.Rect(sprt_rect_x, sprt_rect_y, len_sprt_x, len_sprt_y)) #find sprite you want
-            sprite = sheet.subsurface(sheet.get_clip()) #grab the sprite you want
-            sprites.append(sprite)
-            sprt_rect_x += len_sprt_x
+    len_sprt_x = size_x
+    len_sprt_y = size_y  # размер спрайта
 
-        sprt_rect_y += len_sprt_y
-        sprt_rect_x = 0
-    print sprites
+    sprite = pygame.image.load(file)  # Загружаем файл
+    sprite_rect = sprite.get_rect() # Запрос разрешения файла
+    kartinok_v_stroke=int(sprite_rect.width/size_x)
+    kartinok_v_stolbce = int(sprite_rect.height / size_x)
+    print(kartinok_v_stroke, kartinok_v_stolbce)
+
+    for nomer in range(kartinok_v_stroke):
+
+        for i in range(kartinok_v_stolbce):
+            sprite.set_clip(pygame.Rect(0 + len_sprt_x * nomer, 0 +len_sprt_y*i, len_sprt_x,len_sprt_y ))  # установите текущую область обрезки Поверхности
+            a = sprite.subsurface(sprite.get_clip())  # получаем текущую область обрезки Поверхности
+            sprites.append(a)
+
     return sprites
+
+file ='sara 16x18 source.png'
+
+s2=sprite_map(file,16,18)
+
+print(len(s2))
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+clock = pygame.time.Clock()
+x=0
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    screen.fill((255, 255, 255))
+
+    # for x in range(9):
+    #     screen.blit(s2[x],(20*x+20, 20))
+
+    screen.blit(s2[x], (20, 20))
+    x += 1
+    if x>35: x=0
+
+
+    pygame.display.flip()
+    clock.tick(5)
