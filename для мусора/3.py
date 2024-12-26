cc2a5c33f6b702e07c194fe946fa2e3ba1ba3aa6
master <dimad@udm.ru>
2024-12-26 11:57:33 +0400

    9_3

--- для мусора/3.py
+++ для мусора/3.py
@@ -20,9 +20,10 @@ def sprite_map(file,size_x,size_y):
             sprites.append(a)
 
     return sprites
-a_1=0
+
 fps=60
 file ='sara 16x18 source.png'
+bg = pygame.image.load("bg.jpg")
 
 s2=sprite_map(file,16,18)
 
@@ -31,7 +32,7 @@ print(len(s2))
 # Загрузка спрайтов для анимации
 walk_right = [s2[1],s2[5],s2[9]]
 walk_left = [s2[3],s2[7],s2[11]]
-walk_up = [s2[0],s2[4],s2[8]]
+walk_up = [s2[0],s2[4],s2[8],s2[4]]
 walk_down = [s2[2],s2[6],s2[10]]
 
 # Начальный кадр анимации
@@ -45,7 +46,7 @@ sprite_y = 300
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((900, 900))
 done = False
 clock = pygame.time.Clock()
 x=0
@@ -87,44 +88,49 @@ while not done:
 
 
     # Ограничение координат внутри экрана
-    if sprite_x > 770:
-        sprite_x = 770
-    elif sprite_x < 30:
-        sprite_x = 30
-    if sprite_y > 570:
-        sprite_y = 570
-    elif sprite_y < 30:
-        sprite_y = 30
-    # for x in range(9):
-    #     screen.blit(s2[x],(20*x+20, 20))
+    if sprite_x > 850:
+        sprite_x = 850
+    elif sprite_x < 0:
+        sprite_x = 0
+    if sprite_y > 800:
+        sprite_y = 800
+    elif sprite_y < 0:
+        sprite_y = 0
 
-    screen.blit(s2[x], (20, 20))
+
+    screen.blit(s2[8], (20, 20))
     x += 1
     if x>35: x=0
 
         # Выбор текущего кадра анимации
 
-    current_frame = (current_frame + 1) % len(walk_right)
+
+    # current_frame = (current_frame + 1) % len(walk_right)
 
     # new_image = pygame.transform.scale(image, (image.get_width() // 2, image.get_height() // 2)
 
     if direction == "right":
-        sprite_image = walk_right[current_frame // animation_speed]
+        current_frame = (pygame.time.get_ticks() // 50) % 3
+        sprite_image = walk_right[current_frame ]
 
     elif direction == "up":
-        sprite_image = walk_up[current_frame // animation_speed]
+        current_frame = (pygame.time.get_ticks() // 50) % 3
+        sprite_image = walk_up[current_frame ]
 
     elif direction == "down":
-        sprite_image = walk_down[current_frame // animation_speed]
+        current_frame = (pygame.time.get_ticks() // 50) % 3
+        sprite_image = walk_down[current_frame]
 
     elif direction == "left":
-        sprite_image = walk_left[current_frame // animation_speed]
+        current_frame = (pygame.time.get_ticks() // 50) % 3
+        sprite_image = walk_left[current_frame ]
 
     else:
         current_frame = (current_frame)
         sprite_image = sprite_image
 
         # Отображение спрайта
+    screen.blit(bg, (0, 0))
     sprite_image_1=pygame.transform.scale(sprite_image, (sprite_image.get_width() * 4, sprite_image.get_height() * 4))
     screen.blit(sprite_image_1, (sprite_x, sprite_y))
 
