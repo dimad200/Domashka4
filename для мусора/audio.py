cba705ca9e5358e12490aec2c00f785255d5737d
dimad200 <dimad@udm.ru>
2025-01-25 15:22:25 +0400

    11_3

--- для мусора/audio.py
+++ для мусора/audio.py
@@ -1,88 +1,94 @@
+from multiprocessing.pool import worker
+
 import moviepy as mp
 import os
 import sys
 import pathlib
+import tkinter
 
-os.chdir("G:\курс")
-work_dir=os.getcwd()  # получить  текущую дерикторию
+window = tkinter.Tk()
+def main():
+    try:
+        os.chdir(work_folrer_get.get())
+        work_dir = os.getcwd()  # получить  текущую дерикторию
 
-files_=os.listdir() # получить содержание дирриктории
-name="file_list.txt" # файл куда запишем список всех найденных файлов
+        files_ = os.listdir()  # получить содержание дирриктории
+        name = "file_list.txt"  # файл куда запишем список всех найденных файлов
 
+        with open(name, "w", encoding="utf-8") as file:
+            for k in os.walk(work_dir):
+                for a1_ in k[2]:
+                    file.write(f"{k[0]}\\{a1_}\n")
 
-with open(name, "w", encoding="utf-8") as file:
-    for k in os.walk(work_dir):
-        for a1_ in k[2]:
+        file2 = open("Only_mp4_mp3.txt", 'w', encoding="utf-8")
+        with open(name, "r", encoding="utf-8") as file:
+            while True:
+                # считываем строку
+                line = file.readline()
+                if line.endswith(".mp4\n") or line.endswith(".mp3\n"):
+                    file2.write(line)
+                # прерываем цикл, если строка пустая
+                if not line:
+                    break
+                # выводим строку
+                # print(line.strip())
+        file2.close()
 
-            file.write(f"{k[0]}\\{a1_}\n")
+        with open("Only_mp4_mp3.txt", "r", encoding="utf-8") as file:
+            mp3 = None
+            mp4 = None
+            while True:
+                # считываем строку
+                line = file.readline()
+                if not line:
+                    break
+                if line.endswith(".mp3\n"):
+                    mp3 = line
+                    s2 = "\n"
+                    mp3 = mp3.replace(s2, '')
 
+                elif line.endswith(".mp4\n"):
+                    s2 = "\n"
+                    mp4 = line
+                    mp4 = mp4.replace(s2, '')
 
-file2 = open("Only_mp4_mp3.txt", 'w', encoding="utf-8")
-with open(name, "r" ,encoding="utf-8") as file:
-    while True:
-        # считываем строку
-        line = file.readline()
-        if line.endswith(".mp4\n")  or line.endswith( ".mp3\n"):
-            file2.write(line)
-        # прерываем цикл, если строка пустая
-        if not line:
-            break
-        # выводим строку
-        # print(line.strip())
-file2.close()
+                if not mp3 == None and not mp4 == None:
+                    path = pathlib.Path(mp3)
+                    mp3 = path.name
+                    path = pathlib.Path(mp4)
+                    mp4 = path.name
+                    os.chdir(path.parent)
+                    print(mp3)
+                    print(mp4)
+                    print(path.parent)
 
-with open("Only_mp4_mp3.txt", "r" ,encoding="utf-8") as file:
-    mp3=None
-    mp4=None
-    while True:
-        # считываем строку
-        line = file.readline()
-        if not line:
-            break
-        if line.endswith(".mp3\n"):
-            mp3 =line
-            s2 = "\n"
-            mp3 = mp3.replace(s2, '')
-
-        elif line.endswith(".mp4\n"):
-            s2 = "\n"
-            mp4=line
-            mp4=mp4.replace(s2, '')
-
-        if not mp3==None and not mp4==None:
-            path = pathlib.Path(mp3)
-            mp3=path.name
-            path = pathlib.Path(mp4)
-            mp4=path.name
-            os.chdir(path.parent)
-            print(mp3)
-            print(mp4)
-            print(path.parent)
-
-            audio = mp.AudioFileClip(mp3)
-            video = mp.VideoFileClip(mp4)
-            final_video = video.with_audio(audio)
-            s1 = mp3
-            s2 = ".mp3"
-            s3 = s1.replace(s2, '_final.mp4')
-            mp4=None
-            mp3=None
-            print(s3)
-            final_video.write_videofile(s3)
-
+                    audio = mp.AudioFileClip(mp3)
+                    video = mp.VideoFileClip(mp4)
+                    final_video = video.with_audio(audio)
+                    s1 = mp3
+                    s2 = ".mp3"
+                    s3 = s1.replace(s2, '_final.mp4')
+                    mp4 = None
+                    mp3 = None
+                    print(s3)
+                    final_video.write_videofile(s3)
+    except:
+        pass
 
 
 
-# # Загрузка аудиофайла
-#  print((f'{base_path}Audio/file.mp3'))
-# print(base_path)
-# audio = mp.AudioFileClip('1.mp3')
+work_folrer= tkinter.Label(window, text='папка для работы')
+work_folrer.grid(column=1, row=1)
+
+work_folrer_get = tkinter.Entry(window)
+work_folrer_get.grid(column=1, row=2)
+
+button = tkinter.Button(window, text="Склеить", command=main )
+button.grid(column=1, row=3)
+
+window.title('прога для склейки')
+window.geometry("250x200")
+# window.attributes('-topmost', True)
 #
-# # Загрузка видеофайла
 
-#
-# # Добавление внешнего звука к видео
-# final_video = video.with_audio(audio)
-#
-# # Экспорт итогового видеофайла
-# final_video.write_videofile("output_video.mp4")
+window.mainloop()
---
+++ для мусора/разметка_1.py
@@ -0,0 +1,253 @@
+import tkinter, os
+from pprint import pprint
+from urllib.parse import urlparse
+import requests
+import xml.etree.ElementTree as ET
+
+
+window = tkinter.Tk()
+window.title('прога для удобства')
+window.geometry("500x450")
+window.attributes('-topmost', True)
+
+
+
+
+
+def get_xml_click():
+    print("Папка",folder_get.get())
+    print("файл", entry1.get())
+
+    entry1_1 = entry1.get().replace(".","")
+
+    entry1_1 = entry1_1.replace('"', '')
+    print(entry1_1)
+
+
+
+
+    try:
+        os.chdir(folder_get.get())
+        response = requests.get(url_get_.get())
+        print()
+        with open(entry1_1+".xml", "wb") as f:
+            f.write(response.content)
+
+
+    except:
+        print("Где-то Ошибка!")
+    xml_get.delete(0,len(xml_get.get()))
+    xml_get.insert(0, entry1_1+".xml")
+    xml_folder_get.delete(0,len(xml_folder_get.get()))
+    xml_folder_get.insert(0,folder_get.get())
+
+    entry1.delete(0,len(entry1.get()))
+    url_get_.delete(0,len(url_get_.get()))
+
+def dowenload(file_name):
+    # os.chdir(folder_get.get())
+    curent_dir=os.path.split(file_name)[0]
+    curent_file=os.path.split(file_name)[1]
+    curent_file=curent_file.split(".")[0]
+    print(curent_dir,"+", curent_file)
+    # print(os.path.split(file_name)[0])
+    files_name=os.path.basename(file_name)
+    print(files_name, type(files_name))
+    try:
+        os.chdir(curent_dir)
+
+    except:
+        print(os.curdir)
+
+
+    print(os.getcwd())
+    with open(curent_file+".xml_links.txt", "r", encoding="utf-8") as file:
+        sufix = 0
+        while True:
+            # считываем строку
+            url = file.readline()
+            # print("URL", url)
+            if not url:
+                break
+            response = requests.get(url)
+
+            # получим имя файла
+
+            url = url.split("?")[0]
+            # print("URL",url)
+
+            a = urlparse(url)
+            # print(a.path)  # Output: /kyle/09-09-201315-47-571378756077.jpg
+            # print(os.path.basename(a.path))
+            video_name=os.path.basename(a.path)
+            if video_name.endswith("audio_0.mp4"):
+
+                video_name=curent_file+".mp3"
+                print(video_name)
+
+            elif video_name.endswith(".mp4"):
+                sufix+=1
+                # print(sufix)
+                # video_name=curent_file+"_"+str(sufix)+".mp4"
+                video_name=curent_file+".mp4"
+                print(video_name)
+
+            with open(video_name, "ab") as f:
+                f.write(response.content)
+
+
+def xml_analiz():
+    # print(xml_folder_get.get())
+    try:
+        ch_dir=xml_folder_get.get()
+        os.chdir(ch_dir)
+    except:
+        print("директория не указана!")
+
+    try:
+        os.path.split(xml_get.get())
+        if os.path.split(xml_get.get())[0]=="":
+            print("директория не найдена. используется из поля 'Папка для анализа'")
+            curent_dir = ch_dir
+        else:
+            curent_dir=os.path.split(xml_get.get())[0]
+
+        file=os.path.split(xml_get.get())[1]
+        os.chdir(curent_dir)
+
+        root_node = ET.parse(file).getroot()
+
+        print(root_node)
+        print(root_node.attrib)
+        video={}
+        for i in range(len(root_node[0])):
+            if root_node[0][i].attrib["mimeType"]=="video/mp4":
+                video["width"]= root_node[0][i].attrib["maxWidth"]
+                video["height"]= root_node[0][i].attrib["maxHeight"]
+                video["livel_1"]=i
+
+            if root_node[0][i].attrib["mimeType"] == "audio/mp4":
+                video["audio/mp4"]= i
+
+            print(video)
+
+
+
+            print()
+
+    except:
+        print("что то не так!")
+
+    for i in range(len(root_node[0][video["livel_1"]])):
+
+        if root_node[0][video["livel_1"]][i].attrib["width"] == video["width"] and root_node[0][video["livel_1"]][i].attrib["height"] == video["height"]:
+            video["livel_2"]=i
+            print("TUT       TUT")
+
+    node=root_node[0][video["livel_1"]][video["livel_2"]]
+    node_audio = root_node[0][video["audio/mp4"]]
+    print(node_audio[1][0].tag, node_audio[1][0].attrib)
+    print("metka ya tut ")
+    video["audio/BaseURL"]=node_audio[1][0].text
+    print(video)
+    video["BaseURL"]=node[0].text
+
+
+    # получение сегменотв видио
+
+
+    c = []
+
+    temp_len = len(node[1])
+    print(temp_len)
+    for i in range(1, temp_len):
+        if len(c) == 0:
+            c.append(node[1][i].attrib["media"])
+        else:
+            if c[-1] != node[1][i].attrib["media"]:
+                c.append(node[1][i].attrib["media"])
+    print(c)
+    print("GJHGKUJNTUIOtbniuoyildfuytmgbvitgpytfjk!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    c_1=[]
+    for i in c:
+        print("i=",i)
+        if len(c_1) == 0:
+            c_1.append(i.split("?")[0])
+        else:
+            if c_1[-1] != i.split("?")[0]:
+                c_1.append(i.split("?")[0])
+    print(c_1)
+
+
+
+    video['media'] = c_1
+
+    pprint(video)
+
+    # получение готовых ссылок
+    print(xml_get.get())
+    file_name=xml_get.get()+"_links.txt"
+    print(file_name)
+    with open( file_name, "w", encoding="utf-8") as file:
+        file.write(video["audio/BaseURL"]+"\n")
+        for i in video["media"]:
+            file.write(video['BaseURL']+i+"\n")
+
+
+
+# оформление интерфейса
+
+text = tkinter.Label(window, text='имя для файла ')
+text.grid(column=1, row=1)
+
+entry1 = tkinter.Entry(window)
+entry1.grid(column=1, row=2)
+
+url_get_text = tkinter.Label(window, text='адрес файла ')
+url_get_text.grid(column=1, row=3)
+
+url_get_ = tkinter.Entry(window)
+url_get_.grid(column=1, row=4)
+
+folder_text = tkinter.Label(window, text='папка результата')
+folder_text.grid(column=1, row=5)
+
+folder_get = tkinter.Entry(window)
+folder_get.grid(column=1, row=6)
+
+button = tkinter.Button(window, text="Получить", command=get_xml_click )
+button.grid(column=2, row=3)
+
+xml_for_analiz_folrer= tkinter.Label(window, text='папка для анализа')
+xml_for_analiz_folrer.grid(column=1, row=7)
+
+xml_folder_get = tkinter.Entry(window)
+xml_folder_get.grid(column=1, row=8)
+
+xml_for_analiz= tkinter.Label(window, text='файл для анализа')
+xml_for_analiz.grid(column=1, row=9)
+
+
+
+xml_get = tkinter.Entry(window)
+xml_get.grid(column=1, row=10)
+# xml_get.config(width=200, height=5)
+# вставка начальных данных
+xml_get.insert(0, r"D:\IT\купс\1\Введение Строки байты и кодировки.xml")
+
+button_2 = tkinter.Button(window, text="Проанализировать", command=xml_analiz )
+button_2.grid(column=2, row=7)
+
+button_2 = tkinter.Button(window, text="Скачать", command=lambda: dowenload(xml_get.get()),)
+button_2.grid(column=2, row=8)
+
+
+
+def on_button_click():
+    pass
+
+
+
+
+
+window.mainloop()
