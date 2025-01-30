c8b89855584aa69b2a8f317845d6ffef27f7e73b
dimad200 <dimad@udm.ru>
2025-01-30 12:54:57 +0400

    12_2

--- module_12/module_12_2.py
+++ module_12/module_12_2.py
@@ -13,13 +13,37 @@ class RunnerTest(unittest.TestCase):
         self.runer_2 = Runner("Андрей", 9)
         self.runer_3 = Runner("Ник", 3)
 
+    def test_1(self):
+        t_1 = Tournament(90, self.runer_1, self.runer_3)
+        itog = t_1.start()
+        max_key = 0
+        print(itog.items())
+        for i in itog.keys():
+            max_key = (int(i) if max_key < int(i) else max_key)
+        self.assertTrue(itog[max_key] == "Ник")
+        RunnerTest.all_results["test_1"] = itog
+
+
+    def test_2(self):
+        t_1 = Tournament(90, self.runer_2, self.runer_3)
+        itog = t_1.start()
+        max_key = 0
+        for i in itog.keys():
+            max_key = (int(i) if max_key < int(i) else max_key)
+        self.assertTrue(itog[max_key] == "Ник")
+        RunnerTest.all_results["test_2"] = itog
+
+    def test_3(self):
+        t_1 = Tournament(90, self.runer_1,self.runer_2, self.runer_3)
+        itog = t_1.start()
+        max_key = 0
+        for i in itog.keys():
+            max_key = (int(i) if max_key < int(i) else max_key)
+        self.assertTrue(itog[max_key] == "Ник")
+        RunnerTest.all_results["test_3"] = itog
 
     @classmethod
     def tearDownClass(cls):
-        print(cls.all_results)
-
-    def test_1(self):
-        t_1 = Tournament(90, self.runer_1, self.runer_2)
-        result= t_1.start()
-        print(result)
-        for i in result:
+        for i in cls.all_results.keys():
+            print(f"{(cls.all_results[i])}")
+        print("nrn ",cls.all_results[1])
